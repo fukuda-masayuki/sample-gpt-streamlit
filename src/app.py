515bacd39999ac4dbaa33013392e535981ef3c3a
fukuda-masayuki <fm1146274.fm@gmail.com>
2023-04-23 03:04:48 +0900

    インストール対象を追加

--- src/app.py
+++ src/app.py
@@ -1,3 +1,14 @@
 import streamlit as st
+import requests
 
-st.title('hello')
+st.title('GPTと会話をしよう！！')
+
+title = st.text_input('Movie title', 'Life of Brian')
+
+
+def insert():
+    return st.write("追加")
+
+if st.button('送信'):
+    st.write(title)
+    st.write("こんにちは")
